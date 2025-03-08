e78afcf1ab8d47e6a257191a8fd0872e97a4b89d
maoruihan <798347516@qq.com>
2025-03-08 13:29:52 +0800

    updates

--- example.py
+++ example.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
     single_thread()
     # then run multi thread simulation
     processes = []
-    for i in range(1, 64):
+    for i in range(1, 128):
         p = multiprocessing.Process(target=single_thread, args=(i,))
         processes.append(p)
         p.start()
