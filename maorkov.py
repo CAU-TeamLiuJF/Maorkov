aa6ca64dcac4692fe7a8104dbae97da24f6b1089
maoruihan <798347516@qq.com>
2025-03-24 23:28:30 +0800

    updates

--- maorkov.py
+++ maorkov.py
@@ -261,7 +261,7 @@ class Population:
     def bg_gene_all_positive_percent(
             self,
             first=100,
-            threshold=0.90,
+            threshold=0.95,
             sort_priority="TargetGeneFirst"
     ):
         """
