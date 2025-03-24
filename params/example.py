a59582f1e2b11b8f7d2f2acd2862dadf8b3ee907
maoruihan <798347516@qq.com>
2025-03-24 19:18:19 +0800

    updates

--- params/example.py
+++ params/example.py
@@ -1,7 +1,7 @@
 # total gene number
 TOTAL_GENE_NUM = 100
 # target gene number
-TARGET_GENE_NUM = 5
+TARGET_GENE_NUM = 3
 # max recombination rate
 MAX_RECOMBINATION_RATE = 0.1
 # initial donor population number
@@ -9,7 +9,7 @@ INITIAL_DONOR_NUM = 100
 # initial receptor population number
 INITIAL_RECEPTOR_NUM = 5000
 # background gene frequency
-INITIAL_DONOR_BACKGROUND_GENE_FREQUENCY = [0.2, 0.8]
+INITIAL_DONOR_BACKGROUND_GENE_FREQUENCY = [0, 1.0]
 INITIAL_RECEPTOR_BACKGROUND_GENE_FREQUENCY = [0.8, 0.2]
 # male:female ratio
 CROSS_SEX_RATION = 10
@@ -22,7 +22,7 @@ BACK_CROSS_FATHER_NUM = 50
 # recursive cross father number
 RECURSIVE_FATHER_NUM_LIST = list(range(3, 8))
 # recursive cross max generation
-RECURSIVE_MAX_GENERATION = 6
+RECURSIVE_MAX_GENERATION = 7
 # father numer budget
 FATHER_BUDGET = 35
 # gene percentage of success
